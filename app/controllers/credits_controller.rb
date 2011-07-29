c03b8a42cc4f351247006d6d685634bc9838210e
krypt2005 <krypt2005@gmail.com>
2011-07-29 07:14:09 +0530

    this is my facebook app

---
+++ app/controllers/credits_controller.rb
@@ -0,0 +1,38 @@
+class CreditsController < ApplicationController
+  def create
+    req = Facebook.auth.from_signed_request(params[:signed_request])
+    order = req.data[:credits]
+    res = case params[:method]
+    when 'payments_get_items'
+      {
+        :content => [{
+          :title       => "Donation to fb_graph",
+          :description => "Thanks for your donation!",
+          :price       => 2,
+          :image_url   => "http://www.facebook.com/images/gifts/21.png",
+          :product_url => "http://www.facebook.com/images/gifts/21.png"
+        }]
+      }
+    when 'payments_status_update'
+      {
+        :content => [{
+          :order_id => order[:order_id],
+          :status => :settled
+        }]
+      }
+    end
+    res[:method] = params[:method]
+    logger.info res.to_json
+    render :json => res
+  end
+end
+
+# {
+#     "signed_request" = >"...",
+#     "buyer" = >"579612276",
+#     "receiver" = >"579612276",
+#     "order_id" = >"207732695940804",
+#     "order_info" = >"\"Donation to fb_graph\"",
+#     "test_mode" = >"1",
+#     "method" = >"payments_get_items"
+# }
